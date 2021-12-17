ef1460a80b3b355668ac05a35953333d88b9c76d
Gregor Kovač <kovac.gregi@gmail.com>
2021-12-17 14:55:54 +0100

    Added SDL

---
+++ Makefile
@@ -0,0 +1,2 @@
+EvolutionSimulator:
+	g++ -o src/EvolutionSimulator.out src/*.cpp -I lib -l SDL2-2.0.0
--- src/EvolutionSimulator.cpp
+++ src/EvolutionSimulator.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <vector>
+#include <SDL2/SDL.h>
 #include "../include/NeuralNet.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
     vector<int> topology;
     vector<int> dna;
 
@@ -14,10 +16,57 @@ int main(int argc, char* argv[]) {
 
     dna.push_back(6639); // 0001100111101111
 
-
     NeuralNet net(topology, dna); 
 
     net.printNet();
+
+
+    //Initialize all the systems of SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    //Create a window with a title, "Getting Started", in the centre
+    //(or undefined x and y positions), with dimensions of 800 px width
+    //and 600 px height and force it to be shown on screen
+    SDL_Window* window = SDL_CreateWindow("Getting Started", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+
+    //Create a renderer for the window created above, with the first display driver present
+    //and with no additional settings
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    //Set the draw color of renderer to green
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    //Clear the renderer with the draw color
+    SDL_RenderClear(renderer);
+
+    //Update the renderer which will show the renderer cleared by the draw color which is green
+    SDL_RenderPresent(renderer);
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+    
+    //Destroy the renderer created above
+    SDL_DestroyRenderer(renderer);
+
+    //Destroy the window created above
+    SDL_DestroyWindow(window);
+
+    //Close all the systems of SDL initialized at the top
+    SDL_Quit();
 
     return 0;
 }
